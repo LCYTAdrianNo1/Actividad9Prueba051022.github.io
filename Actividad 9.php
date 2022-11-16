4e46e8ad0995158834324a3817efb8a2704a66f1
LCYTAdrianNo1 <80604147+LCYTAdrianNo1@users.noreply.github.com>
2022-11-15 21:01:09 -0500

    Inicial V1.0

---
+++ Actividad 9.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+     <title>¡Una página con estilo!</title>
+   </head>
+   <link href="estilos.css" type="text/css" rel="stylesheet"/>
+   <body>
+   
+  	  <!-- Contenido Principal -->
+  	 <h1>¡Está es una página con mucho estilo!
+
+     <img src="https://images.ecestaticos.com/x5b-19QHQb3DC_dxW6lS1r3iPGc=/0x0:1772x1691/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Fa6c%2Ff0d%2Fc18%2Fa6cf0dc186b5c11ed0ec74d0c7b581df.jpg"
+     alt="Imagen De Animales"
+     width="200"
+     height="100"
+     title="Imagen de animales">
+
+     </h1>
+
+  	<h2>Bienvenidos a mi página con mucho estilo.</h2>
+
+  	 <p>Aún no tiene imágenes pero si tiene estilo.Tambien tiene ligas, aunque no llevan a ningún lado</p>
+  	
+     <p>Pronto tendrá más contenido, ¡así que tengan paciencia!</p>
+  
+   <!-- Site navigation menu -->
+   <ul class="navbar">
+     <li><a href="http://localhost/proyectoscx/Actividad%20051022/Actividad%209.php">Inicio</a>
+     <li><a href="https://www.expertoanimal.com/animales-marinos-tipos-caracteristicas-y-ejemplos-20710.html">Animales marinos</a>
+     <li><a href="https://concepto.de/animales-terrestres/">Animales terrestres</a>
+     <li><a href="https://www.expertoanimal.com/animales-aereos-ejemplos-y-caracteristicas-24064.html">Animales voladores</a>
+   </ul>
+  
+   <footer>Hecha con amor el <?php echo date("d/m/Y") ?> <!-- CODIGO DE PHP Para la fecha cx --> con mucho amor.</footer>
+   
+  </body>
+ </html>
